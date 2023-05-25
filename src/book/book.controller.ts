1e71430e7cadf55125bc57cc4907892a5909d71c
janani-maya <janimaya13@gmail.com>
2023-05-25 09:07:32 +0530

    feat(book) : Added API to get all books

---
+++ src/book/book.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { Query as ExpressQuery } from 'express-serve-static-core';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+@Controller('book')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks(@Query() query: ExpressQuery): Promise<Book[]> {
+    return this.bookService.findAll(query);
+  }
+}
---
+++ src/book/book.service.ts
@@ -0,0 +1,36 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, isValidObjectId } from 'mongoose';
+import { Query } from 'express-serve-static-core';
+import { Book } from './schemas/book.schema';
+
+@Injectable()
+export class BookService {
+  constructor(
+    @InjectModel(Book.name)
+    private bookModel: Model<Book>,
+  ) {}
+
+  async findAll(query: Query): Promise<Book[]> {
+    const resPerPage = 2;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+    const books = await this.bookModel
+      .find({ ...keyword })
+      .limit(resPerPage)
+      .skip(skip);
+    return books;
+  }
+}
